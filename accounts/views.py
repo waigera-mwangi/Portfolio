d970a88d6d9001f998f839000c57d0b8ee83d5d6
waigera mwangi <waigeram019@gmail.com>
2023-12-28 17:59:46 +0300

    Initial commit

--- accounts/views.py
+++ accounts/views.py
@@ -5,4 +5,6 @@ from django.views.generic import TemplateView
 class IndexPageView(TemplateView):
     template_name = 'index.html'
     
-    
+def index(request):
+    linkedin_profile_url = "https://www.linkedin.com/in/waigera-mwangi-995944223"
+    return render(request, 'index.html', {'linkedin_profile_url': linkedin_profile_url})
